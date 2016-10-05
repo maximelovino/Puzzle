068c0c309b86b86e94fc4171cc62d9205288052d
Lovino Maxime <maximelovino@gmail.com>
2016-10-05 17:52:34 +0200

    Added menu, parent states and randomStates, TODO: unstack path after finding result

--- src/ch/hepia/algo/puzzle/game/Solver.java
+++ src/ch/hepia/algo/puzzle/game/Solver.java
@@ -8,21 +8,38 @@ import java.util.concurrent.LinkedBlockingQueue;
 public class Solver {
 
 	public static void main (String[] args) {
-		String st = "820347516";
-		int size = 3;
-		State initialState = new State(st,size,0);
-		System.out.println("initial state");
-		System.out.println(initialState);
-
-		State finishedState = blindSolve(initialState,size,false);
-
-		System.out.println();
-		if (finishedState!=null) {
-			System.out.println(finishedState);
-			System.out.println("Cost "+finishedState.getCost());
-		}else{
-			System.out.println("no solution was found");
+		if (args.length < 3){
+			System.out.println("Please use these programs with the following arguments");
+			System.out.println("<Search type> <Initial State> <Goal State>");
+			System.out.println();
+			System.out.print("\t");
+			System.out.println("<Search type>: blind, cachedBlind, heuristics");
+			System.out.print("\t");
+			System.out.println("<Initial State>: Write the cases from top to bottom, left to right, first one top left, last one bottom right, 0 for the empty, or use 'RANDOM n' where n is the size of nxn puzzle");
+			System.out.print("\t");
+			System.out.println("<Goal State>: Same as <Initial State> but you can use GOAL to set the goal state as the logical goal state for that size");
+			return;
 		}
+
+
+		State st = State.getRandomState(3);
+		System.out.println(st);
+
+//		String st = "820347516";
+//		int size = 3;
+//		State initialState = new State(st,size,0,null);
+//		System.out.println("initial state");
+//		System.out.println(initialState);
+//
+//		State finishedState = blindSolve(initialState,size,false);
+//
+//		System.out.println();
+//		if (finishedState!=null) {
+//			System.out.println(finishedState);
+//			System.out.println("Cost "+finishedState.getCost());
+//		}else{
+//			System.out.println("no solution was found");
+//		}
 
 	}
 
--- src/ch/hepia/algo/puzzle/utils/State.java
+++ src/ch/hepia/algo/puzzle/utils/State.java
@@ -1,24 +1,41 @@
 package ch.hepia.algo.puzzle.utils;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Random;
 
+/**
+ * Class representing a state of the game
+ * <p>
+ * State is represented as a string, cost is the number of moves to get to the state
+ */
 public class State {
 	private String state;
 	private int n;
 	private int indexOfEmpty;
 	private int cost;
+	private State parentState;
 
-	public State (String state, int n, int cost) {
-		if (state.length()!= Math.pow(n,2))
+	/**
+	 * Default constructor for State
+	 *
+	 * @param state The state, as a string
+	 * @param n     The size of the square (nxn)
+	 * @param cost  The number of moves to get to the state
+	 */
+	public State (String state, int n, int cost, State parentState) {
+		if (state.length() != Math.pow(n, 2))
 			throw new IllegalArgumentException("invalid state, size not compatible");
 
 		this.state = state;
 		this.n = n;
 		this.cost = cost;
 		this.indexOfEmpty = this.state.indexOf('0');
+		this.parentState = parentState;
 	}
 
-	public ArrayList<State> successors(){
+	public ArrayList<State> successors () {
 		ArrayList<State> successors = new ArrayList<>();
 
 		State temp;
@@ -39,32 +56,32 @@ public class State {
 		return successors;
 	}
 
-	private State moveLeft(){
-		if (this.indexOfEmpty % n ==0)
+	private State moveLeft () {
+		if (this.indexOfEmpty % n == 0)
 			return null;
 
-		return swapEmptyWith(indexOfEmpty-1);
+		return swapEmptyWith(indexOfEmpty - 1);
 	}
 
-	private State moveRight(){
-		if(this.indexOfEmpty % n == n-1)
+	private State moveRight () {
+		if (this.indexOfEmpty % n == n - 1)
 			return null;
 
-		return swapEmptyWith(indexOfEmpty+1);
+		return swapEmptyWith(indexOfEmpty + 1);
 	}
 
-	private State moveUp(){
-		if (this.indexOfEmpty-n < 0)
+	private State moveUp () {
+		if (this.indexOfEmpty - n < 0)
 			return null;
 
-		return swapEmptyWith(indexOfEmpty-n);
+		return swapEmptyWith(indexOfEmpty - n);
 	}
 
-	private State moveDown(){
-		if (this.indexOfEmpty+n > this.state.length()-1)
+	private State moveDown () {
+		if (this.indexOfEmpty + n > this.state.length() - 1)
 			return null;
 
-		return swapEmptyWith(indexOfEmpty+n);
+		return swapEmptyWith(indexOfEmpty + n);
 	}
 
 	@Override
@@ -72,22 +89,22 @@ public class State {
 		return this.hashCode() == obj.hashCode();
 	}
 
-	private State swapEmptyWith(int newIndex){
+	private State swapEmptyWith (int newIndex) {
 		char toSwapWith = this.state.charAt(newIndex);
 
-		String s1 = this.state.substring(0,Math.min(newIndex,this.indexOfEmpty));
-		String s2 = this.state.substring(Math.min(newIndex,this.indexOfEmpty)+1,Math.max(newIndex,this.indexOfEmpty));
-		String s3 = this.state.substring(Math.max(newIndex,this.indexOfEmpty)+1);
+		String s1 = this.state.substring(0, Math.min(newIndex, this.indexOfEmpty));
+		String s2 = this.state.substring(Math.min(newIndex, this.indexOfEmpty) + 1, Math.max(newIndex, this.indexOfEmpty));
+		String s3 = this.state.substring(Math.max(newIndex, this.indexOfEmpty) + 1);
 
 		String newString;
 
-		if (this.indexOfEmpty<newIndex){
-			newString = s1+toSwapWith+s2+'0'+s3;
-		}else{
-			newString = s1+'0'+s2+toSwapWith+s3;
+		if (this.indexOfEmpty < newIndex) {
+			newString = s1 + toSwapWith + s2 + '0' + s3;
+		} else {
+			newString = s1 + '0' + s2 + toSwapWith + s3;
 		}
 
-		return new State(newString,this.n, this.cost+1);
+		return new State(newString, this.n, this.cost + 1, this);
 	}
 
 	public String getState () {
@@ -121,21 +138,43 @@ public class State {
 
 			str = this.state.charAt(i) == '0' ? str + "-" : str + this.state.charAt(i);
 
-			if (i != this.state.length()-1)
+			if (i != this.state.length() - 1)
 				str += "\t";
 		}
 
 		return str;
 	}
 
-	public static State getGoalState (int n){
+	public State getParentState () {
+		return parentState;
+	}
+
+	public static State getGoalState (int n) {
 		String str = "";
 
-		for (int i = 0; i < Math.pow(n,2)-1; i++) {
-			str += String.valueOf(i+1);
+		for (int i = 0; i < Math.pow(n, 2) - 1; i++) {
+			str += String.valueOf(i + 1);
 		}
 		str += '0';
 
-		return new State(str,n,0);
+		return new State(str, n, 0, null);
+	}
+
+	public static State getRandomState (int n) {
+		String str = "";
+		Random rand = new Random();
+		ArrayList<Integer> values = new ArrayList<>();
+
+		for (int i = 0; i < Math.pow(n, 2); i++) {
+			values.add(i);
+		}
+
+		for (int i = 0; i < Math.pow(n, 2); i++) {
+			int index = rand.nextInt(values.size());
+			int temp = values.get(index);
+			values.remove(index);
+			str += String.valueOf(temp);
+		}
+		return new State(str, n, 0, null);
 	}
 }
